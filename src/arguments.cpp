4cc9e15707f21dfd1694fb917377c24c87a31787
Radim Mifka <radimmifka@gmail.com>
2024-03-18 18:42:01 +0100

    feat: add arguments from assignment

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -4,10 +4,19 @@ std::unordered_map<std::string, std::string> parse_arguments(int argc,
                                                              char* argv[]) {
   ArgumentParser::Parser parser;
 
-  parser.addArgument("NUMBER", ArgumentParser::Type::UINT8, "Number of items");
+  parser.addArgument("--transport", "-t", ArgumentParser::Type::STRING,
+                     "Transport protocol", true, "", {"UDP", "TCP"});
+
+  parser.addArgument("--server", "-s", ArgumentParser::Type::HOST,
+                     "Transport protocol", true);
   parser.addArgument("--port", "-p", ArgumentParser::Type::UINT16,
-                     "Port number", "4567");
-  parser.addArgument("COUNT", ArgumentParser::Type::UINT8, "Count of items");
+                     "Port number", false, "4567");
+
+  parser.addArgument("--udptimeout", "-d", ArgumentParser::Type::UINT16,
+                     "UDP confirmation timeout", false, "250");
+
+  parser.addArgument("--retries", "-r", ArgumentParser::Type::UINT8,
+                     "Maximum number of UDP retransmissions", false, "3");
 
   return parser.parse(argc, argv);
 }
