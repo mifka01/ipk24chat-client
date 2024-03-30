2cdd803bbd7f5129356e09d1516b0cf0d8eb381c
Radim Mifka <radimmifka@gmail.com>
2024-03-30 17:20:19 +0100

    feat: add udp header

---
+++ include/Protocol/UDP.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "Message/Message.hpp"
+#include "Protocol.hpp"
+
+namespace Protocol {
+class UDP : public Protocol {
+ public:
+  UDP(Client::Session& session) : Protocol(session){};
+  inline int socketType() override { return SOCK_DGRAM; }
+  inline std::string toString() override { return "UDP"; }
+
+  void send(int socket, std::unique_ptr<Message::Message> message) override;
+  std::unique_ptr<Message::Message> receive(int socket) override;
+};
+
+}  // namespace Protocol
