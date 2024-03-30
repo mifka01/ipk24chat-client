5130d9d04d9344a033edd4dc89b9f3c4a9b33e46
Radim Mifka <radimmifka@gmail.com>
2024-03-30 17:05:08 +0100

    fix: change message type to support udp

--- include/Message/Type.hpp
+++ include/Message/Type.hpp
@@ -1,6 +1,14 @@
 #pragma once
 namespace Message {
 
-enum class Type { ERR, CONFIRM, REPLY, AUTH, JOIN, MSG, BYE };
+enum class Type {
+  CONFIRM = 0x00,
+  REPLY = 0x01,
+  AUTH = 0x02,
+  JOIN = 0x03,
+  MSG = 0x04,
+  ERR = 0xFE,
+  BYE = 0xFF
+};
 
 }
